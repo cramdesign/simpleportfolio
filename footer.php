ba8f2de972b88a4d3e2c69f586668c70c01a2230
cramdesign <mcram@Cram-SSU-iMac.local>
2015-03-13 22:51:56 -0400

    updates

--- footer.php
+++ footer.php
@@ -1,18 +1,20 @@
-</main>
+</div><!-- content -->
 
 <footer id="footer">
 <div class="row">
 		
 	<h3 class="description"><?php bloginfo( "description" ); ?></h3>
 	
-</div><!-- row -->	
+</div>	
 </footer>
 	
+</main>
+
 <?php 
 	/*
-	Like wp_head(), wp_footer() allows Wordpress to do some necessary work in the footer of the theme.
-	For example, sometimes javascript is loaded in the footer rather than the head. 
-	Include this function call in every theme.	
+		Like wp_head(), wp_footer() allows Wordpress to do some necessary work in the footer of the theme.
+		For example, sometimes javascript is loaded in the footer rather than the head. 
+		Include this function call in every theme.	
 	*/
 	wp_footer(); 
 ?>
